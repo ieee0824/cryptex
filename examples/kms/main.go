deedc0dffe12af0d84146f8fa7d70e916ba86ba5
ieee0824 <ieee0824@ezweb.ne.jp>
2017-10-11 12:55:34 +0900

    update sample

--- examples/kms/main.go
+++ examples/kms/main.go
@@ -3,21 +3,35 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+
 	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/ieee0824/cryptex"
 	"github.com/ieee0824/cryptex/kms"
+	"github.com/joho/godotenv"
+	"github.com/ieee0824/getenv"
+	"github.com/aws/aws-sdk-go/aws"
 )
+
+var (
+	KMS_KEY_ID string
+)
+
+func init() {
+	godotenv.Load(".env")
+	KMS_KEY_ID = getenv.String("KMS_KEY_ID")
+}
 
 func main() {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		AssumeRoleTokenProvider: stscreds.StdinTokenProvider,
 		SharedConfigState:       session.SharedConfigEnable,
 	}))
+	sess.Config.Region = aws.String(getenv.String("AWS_REGION"))
 
 	kmsClient := kms.New(sess)
 
-	kmsClient.SetKey("kms key id")
+	kmsClient.SetKey(KMS_KEY_ID)
 
 	plainMap := map[string]interface{}{
 		"hoge":  "huga",
